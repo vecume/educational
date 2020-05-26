7b946d6407bded3242677ada9d60f2d8fcf4a795
vecume <shahzod.bek200109@gmail.com>
2020-05-27 01:05:16 +0500

    Add sliders

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const elHeaderSliderWrapper = $('.header__slider');
 const elFeedbacksSliderWrapper = $('.feedbacks__slider');
+const elPartnersSliderWrapper = $('.partners-box');
+const elGallerySliderWrapper = $('.gallery__slider');
+
 elHeaderSliderWrapper.slick({
   nextArrow: '<button type="button" class="btn header-next"><img src="img/header-a-right.png" alt=""></button>',
   prevArrow: '<button type="button" class="btn header-prev"><img src="img/header-a-left.png" alt=""></button>'
@@ -9,5 +12,20 @@ elFeedbacksSliderWrapper.slick({
   slidesToShow: 2,
   slidesToScroll: 2,
   arrows: false,
-  dots: true
+  dots: true,
+  dotsClass: 'slider-controllers'
+});
+
+elGallerySliderWrapper.slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: '<button type="button" class="btn header-next"><img src="img/arrow-right.png" alt=""></button>',
+  prevArrow: '<button type="button" class="btn header-prev"><img src="img/arrow-left.png" alt=""></button>'
+});
+
+elPartnersSliderWrapper.slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  nextArrow: '<button type="button" class="btn header-next"><img src="img/arrow-right.png" alt=""></button>',
+  prevArrow: '<button type="button" class="btn header-prev"><img src="img/arrow-left.png" alt=""></button>'
 });
